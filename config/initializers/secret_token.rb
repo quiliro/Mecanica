776e18f59e74302393230d4dd914296e3e56139b
Quiliro Ordoñez <quiliro@congresolibre.org>
2011-04-28 11:22:05 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mecanica::Application.config.secret_token = '3a919a617499fa8682421f2a106df04736574b015ecaa6a29e0adc9baf6f3b5327d4ee077a1cbf7fb735d12299ad46d34dce0292c3bafebc870133c1640a8133'
